58e1f6f074b34840c6a5fa7364ca7a416daaa31d
Nive <ian1s@Kabuto.localdomain>
2021-06-23 14:37:32 -0300

    first commit

---
+++ lib/wabanex/user.ex
@@ -0,0 +1,28 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @fields [:email, :password, :name]
+
+  # struct that User module will follow
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  # build a changeset based on a map by given types
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/^[\w.!#$%&’*+\-\/=?\^`{|}~]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/i)
+    |> unique_constraint([:name, :email])
+  end
+end
